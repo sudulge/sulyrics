0f9d7e18d8485d1c8415337a14adbf79eb24a89b
sudulge <hhs0888@naver.com>
2022-04-17 02:00:24 +0900

    add lunch

--- sulyrics.py
+++ sulyrics.py
@@ -4,8 +4,8 @@ import discord
 from discord.ext import commands
 from discord.commands import Option
 from datetime import datetime
-# from pytz import timezone
-# from neispy import Neispy
+from pytz import timezone
+from neispy import Neispy
 import time
 import random
 import os
@@ -119,39 +119,39 @@ async def pinMessage(ctx, message_id: Option(str, "메시지 아이디 입력"))
     await message.pin()
 
 
-# @bot.slash_command(name="급식", guild_ids=[723892698435551324], description="급식")
-# async def lunch(ctx, school: Option(str, "학교 이름 입력")):
-#     today = str(datetime.now(timezone('Asia/Seoul')))[:10].replace('-', '')
-#     neis_key = os.environ["NEIS_API_KEY"]
-#
-#     async with Neispy(KEY=neis_key) as neis:
-#         try:
-#             scinfo = await neis.schoolInfo(SCHUL_NM=school)
-#             AE = scinfo[0].ATPT_OFCDC_SC_CODE
-#             SE = scinfo[0].SD_SCHUL_CODE
-#             SCHUL_NM = scinfo[0].SCHUL_NM
-#
-#         except:
-#             await ctx.respond("학교를 찾을 수 없습니다")
-#
-#         else:
-#             try:
-#                 scmeal = await neis.mealServiceDietInfo(AE, SE, MLSV_YMD=f"{today}")
-#                 meal = scmeal[0].DDISH_NM.replace("<br/>", "\n")
-#
-#                 replacelist = '01234567890./'
-#
-#                 for i in replacelist:
-#                     meal = meal.replace(i, '')
-#                 lunchlist = meal
-#
-#             except:
-#                 lunchlist = '오늘의 점심은?'  # 오류 방지용 문구
-#
-#             embed = discord.Embed(title=f"{SCHUL_NM} {today[4:6]}월 {today[6:8]}일 중식", color=0xf5a9a9)
-#             embed.add_field(name="​", value=f"{lunchlist}", inline=False)
-#             embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/769489028357160990.png?v=1")
-#             await ctx.respond(embed=embed)
+@bot.slash_command(name="급식", guild_ids=[723892698435551324], description="급식")
+async def lunch(ctx, school: Option(str, "학교 이름 입력")):
+    today = str(datetime.now(timezone('Asia/Seoul')))[:10].replace('-', '')
+    neis_key = os.environ["NEIS_API_KEY"]
+
+    async with Neispy(KEY=neis_key) as neis:
+        try:
+            scinfo = await neis.schoolInfo(SCHUL_NM=school)
+            AE = scinfo[0].ATPT_OFCDC_SC_CODE
+            SE = scinfo[0].SD_SCHUL_CODE
+            SCHUL_NM = scinfo[0].SCHUL_NM
+
+        except:
+            await ctx.respond("학교를 찾을 수 없습니다")
+
+        else:
+            try:
+                scmeal = await neis.mealServiceDietInfo(AE, SE, MLSV_YMD=f"{today}")
+                meal = scmeal[0].DDISH_NM.replace("<br/>", "\n")
+
+                replacelist = '01234567890./'
+
+                for i in replacelist:
+                    meal = meal.replace(i, '')
+                lunchlist = meal
+
+            except:
+                lunchlist = '오늘의 점심은?'  # 오류 방지용 문구
+
+            embed = discord.Embed(title=f"{SCHUL_NM} {today[4:6]}월 {today[6:8]}일 중식", color=0xf5a9a9)
+            embed.add_field(name="​", value=f"{lunchlist}", inline=False)
+            embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/769489028357160990.png?v=1")
+            await ctx.respond(embed=embed)
 
 @bot.slash_command(name="로또", guild_ids=[723892698435551324], description="로또")
 async def lotto(ctx):
