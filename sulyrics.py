4b5ed4eec0de6784e1d514ec8f1cd10bbcd0e223
sudulge <hhs0888@naver.com>
2022-04-24 09:38:14 +0900

    숫자야구 기록 추가

--- sulyrics.py
+++ sulyrics.py
@@ -6,8 +6,10 @@ from discord.commands import Option
 from datetime import datetime
 from pytz import timezone
 from neispy import Neispy
+import webbrowser
 import time
 import random
+import pickle
 import os
 import re
 
@@ -73,7 +75,7 @@ def checkInput(input, tries):
 
     return True
 
-def NBhit(HRnumber, input, tries, ctx):
+def NBhit(HRnumber, input, tries, tries_list, ctx):
     valid = checkInput(input, tries)
     
     if type(valid)==str:
@@ -91,6 +93,7 @@ def NBhit(HRnumber, input, tries, ctx):
 
     if strike == 4:
         result = f"홈런! {len(tries)}회 만에 맞추셨습니다"
+        NBsolecheck(tries, tries_list, ctx)
         NBend()
     elif strike == 0 and ball == 0:
         result = "아웃"
@@ -98,6 +101,45 @@ def NBhit(HRnumber, input, tries, ctx):
         result = f"{input}: {strike}S {ball}B"
         
     return result, f"{input}: {strike}S {ball}B ⠀({ctx.author.display_name})\n"
+
+def NBsolecheck(tries, tries_list, ctx):  # 마지막으로 입력한 사람이 혼자 했는지 체크 
+    author = ctx.author.display_name
+    if tries_list.count(author) == len(tries)-1:
+        NBaddhistory(tries, ctx)
+    return
+
+def NBaddhistory(tries, ctx):
+    with open("data/NBhistory.pickle", "rb") as f: # 깃허브에 올릴때는 sulyrics/ 빼기
+        history = pickle.load(f)
+
+    for dic in history: # 있던 사람
+        if ctx.author.id in dic.values():
+            if len(tries) in dic['로그']:
+                dic['로그'][len(tries)] += 1
+            else:
+                dic['로그'][len(tries)] = 1
+            dic['로그'] = dict(sorted(dic['로그'].items()))
+            with open("data/NBhistory.pickle", "wb") as f:
+                pickle.dump(history, f)
+            return
+
+    history.append({"id": ctx.author.id, "이름": ctx.author.name, "서버닉네임": ctx.author.display_name, "로그": {len(tries): 1}}) # 새로운 사람
+    with open("data/NBhistory.pickle", "wb") as f:
+        pickle.dump(history, f)
+    return
+
+def NBshowhistory(ctx):
+    with open("data/NBhistory.pickle", "rb") as f:
+        history = pickle.load(f)
+    
+    for dic in history:
+        if ctx.author.id in dic.values():
+            history = ''
+            for key, value in dic['로그'].items():
+                history += (f"{key}회:".ljust(4) + f" {value:2d}번\n")
+            return history
+
+    return False
 
 def NBend():
     global HRnumber, tries
@@ -151,13 +193,6 @@ async def userInfo(ctx):
     embed.set_thumbnail(url=ctx.author.display_avatar)
     await ctx.respond(embed=embed)
 
-
-@bot.slash_command(name="오늘의라인", guild_ids=[723892698435551324], description="롤 라인 뽑기")
-async def lolLane(ctx):
-    lane = ["탑","미드","원딜","서폿","정글"]
-    await ctx.respond(f"오늘의 라인은`{lane[random.randint(0, 4)]}`", ephemeral=True)
-
-
 @bot.slash_command(name="삭제", guild_ids=[723892698435551324], description="메시지 삭제")
 async def deleteMessage(ctx, limit: Option(int, "지울 개수 입력")):
     await ctx.channel.purge(limit=limit, check=lambda msg: not msg.pinned)
@@ -168,6 +203,19 @@ async def pinMessage(ctx, message_id: Option(str, "메시지 아이디 입력"))
     message = await ctx.fetch_message(int(message_id))
     await message.pin()
 
+@bot.slash_command(name="구글", guild_ids=[723892698435551324], description="구글 검색")
+async def googlesearch(ctx, query: Option(str, "검색 내용 입력")):
+    await ctx.respond(f'https://www.google.co.kr/search?q={query.strip().replace(" ", "%20")}')
+
+@bot.slash_command(name="브라우저열기", guild_ids=[723892698435551324], description="구글 검색")
+async def googlesearch(ctx,):
+    url = "https://www.google.co.kr/"
+    webbrowser.open(url)
+
+@bot.slash_command(name="오늘의라인", guild_ids=[723892698435551324], description="롤 라인 뽑기")
+async def lolLane(ctx):
+    lane = ["탑","미드","원딜","서폿","정글"]
+    await ctx.respond(f"오늘의 라인은`{lane[random.randint(0, 4)]}`", ephemeral=True)
 
 @bot.slash_command(name="급식", guild_ids=[723892698435551324], description="급식")
 async def lunch(ctx, school: Option(str, "학교 이름 입력")):
@@ -217,40 +265,51 @@ async def lotto(ctx):
 
 
 @bot.slash_command(name="숫자야구", guild_ids=[723892698435551324], description="숫자야구")
-async def numBaseball(ctx, choice: Option(str, "숫자야구", choices=["시작", "도움말"])):
+async def numBaseball(ctx, choice: Option(str, "숫자야구", choices=["시작", "기록", "도움말"])):
     if choice == "시작":
         global HRnumber, tries, tries_list
         HRnumber, tries, tries_list = NBstart()
         print(HRnumber)
         await ctx.respond("`숫자를 초기화했습니다.\n/야 명령어를 통해 숫자를 입력해 주세요.`")
+    elif choice == "기록":
+        history = NBshowhistory(ctx)
+        NBembed = discord.Embed(color=0xf5a9a9)
+        NBembed.title = f"{ctx.author.display_name}"
+        NBembed.description = "================================="
+        if history:
+            NBembed.add_field(name="기록", value=f"```{history}```")
+        else:
+            NBembed.add_field(name="기록", value=f"숫자야구 플레이 기록이 없습니다.")
+        await ctx.respond(embed=NBembed)
     elif choice == "도움말":
-        NBHembed = discord.Embed(color=0xf5a9a9)
-        NBHembed.title = "숫자야구"
-        NBHembed.description = "================================="
-        NBHembed.add_field(name="규칙", value=
+        NBembed = discord.Embed(color=0xf5a9a9)
+        NBembed.title = "숫자야구"
+        NBembed.description = "================================="
+        NBembed.add_field(name="규칙", value=
         "0~9 사이의 중복되지 않는 네자리 수\n\
         숫자는 맞지만 위치가 틀렸을 경우 '볼'\n\
         숫자와 위치가 모두 맞았을 경우 '스트라이크'\n\
         맞는 숫자가 한 개도 없을 경우 '아웃'\n")
-        await ctx.respond(embed=NBHembed)
+        await ctx.respond(embed=NBembed)
 
 
 @bot.slash_command(name="야", guild_ids=[723892698435551324], description="숫자야구")
 async def numBaseballInput(ctx, input: Option(str, "숫자 입력")):
     try:
         global tries_list, msg
-        result, log = NBhit(HRnumber, input, tries, ctx)
+        result, log = NBhit(HRnumber, input, tries, tries_list, ctx)
         tries_list += log
         NBembed = discord.Embed(color=0xf5a9a9)
         NBembed.title = result
         NBembed.description = "=========================="
-        NBembed.add_field(name="로그", value=tries_list)
+        NBembed.add_field(name="로그", value=f"```{tries_list}```")
         try:
             await msg.delete_original_message()
         finally:
             msg = await ctx.respond(embed=NBembed)
+            print(tries_list)
     except NameError as e:
-        if 'msg' in str(e):
+        if 'msg' in str(e): # 처음 시작 하면 msg가 없음
             pass
         else:
             await ctx.respond("`/숫자야구 명령어를 통해 숫자를 초기화 해주세요.`")
@@ -261,7 +320,7 @@ async def help(ctx):
     Hembed = discord.Embed(color=0xf5a9a9)
     Hembed.title = "수리릭"
     Hembed.set_thumbnail(url="https://cdn.discordapp.com/attachments/731547490347909120/941329421552467998/c39add417a556179.png")
-    Hembed.add_field(name="명령어 리스트", value="`정보` `자기소개` \n `삭제` `고정` \n `급식` `로또` `숫자야구` `오늘의라인`")
+    Hembed.add_field(name="명령어 리스트", value="`정보` `자기소개` \n `삭제` `고정` \n `급식` `로또` `숫자야구` `오늘의라인` `구글`")
     await ctx.respond(embed=Hembed)
 
 
